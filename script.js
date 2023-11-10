ebf1cef19790124c1c461f292a173ccb20816fae
Fabio Barbosa <116105992+devsfabio@users.noreply.github.com>
2023-11-10 19:58:28 -0300

    resolucao

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const btnFale = document.querySelector('.btn-fale-conosco');
 const formulario = document.querySelector('.fale-conosco');
 const mascara = document.querySelector('.mascara-form');
 function handleClick() {
-  formulario.style.left = '650px';
+  formulario.style.left = '780px';
   mascara.style.visibility = 'visible';
 }
 
 function sumirForm() {
-  formulario.style.left = '-340px';
+  formulario.style.left = '-200px';
   mascara.style.visibility = 'hidden';
 }
